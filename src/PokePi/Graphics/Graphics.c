5a962a305de5a61d757dbe5660501ea15352296a
Bastien Saidi <saidi.ireland@gmail.com>
2021-10-07 00:07:38 +0100

    Testing new content

--- src/PokePi/Graphics/Graphics.c
+++ src/PokePi/Graphics/Graphics.c
@@ -1,37 +1,96 @@
 #include "Graphics.h"
 
-void PokePi_GUI_init(){
-    /*Create a container with ROW flex direction*/
-    lv_obj_t * cont_row = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(cont_row, 300, 75);
-    lv_obj_align(cont_row, LV_ALIGN_TOP_MID, 0, 5);
-    lv_obj_set_flex_flow(cont_row, LV_FLEX_FLOW_ROW);
-
-    /*Create a container with COLUMN flex direction*/
-    lv_obj_t * cont_col = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(cont_col, 200, 150);
-    lv_obj_align_to(cont_col, cont_row, LV_ALIGN_OUT_BOTTOM_MID, 0, 5);
-    lv_obj_set_flex_flow(cont_col, LV_FLEX_FLOW_COLUMN);
-
-    uint32_t i;
-    for(i = 0; i < 10; i++) {
-        lv_obj_t * obj;
-        lv_obj_t * label;
-
-        /*Add items to the row*/
-        obj= lv_btn_create(cont_row);
-        lv_obj_set_size(obj, 100, LV_PCT(100));
-
-        label = lv_label_create(obj);
-        lv_label_set_text_fmt(label, "Item: %d", i);
-        lv_obj_center(label);
-
-        /*Add items to the column*/
-        obj = lv_btn_create(cont_col);
-        lv_obj_set_size(obj, LV_PCT(100), LV_SIZE_CONTENT);
-
-        label = lv_label_create(obj);
-        lv_label_set_text_fmt(label, "Item: %d", i);
-        lv_obj_center(label);
+static lv_obj_t * kb;
+static lv_obj_t * ta;
+static void kb_event_cb(lv_obj_t * keyboard, lv_event_t e)
+{
+    lv_keyboard_def_event_cb(kb, e);
+    if(e == LV_EVENT_CANCEL) {
+        lv_keyboard_set_textarea(kb, NULL);
+        lv_obj_del(kb);
+        kb = NULL;
     }
 }
+static void kb_create(void)
+{
+    kb = lv_keyboard_create(lv_scr_act(), NULL);
+    lv_keyboard_set_cursor_manage(kb, true);
+    lv_obj_set_event_cb(kb, kb_event_cb);
+    lv_keyboard_set_textarea(kb, ta);
+
+}
+static void ta_event_cb(lv_obj_t * ta_local, lv_event_t e)
+{
+    if(e == LV_EVENT_CLICKED && kb == NULL) {
+        kb_create();
+    }
+}
+
+static void exit_button_event_handler(lv_obj_t * obj, lv_event_t event)
+{
+    if(event == LV_EVENT_VALUE_CHANGED) {
+        printf("Button: %s\n", lv_msgbox_get_active_btn_text(obj));
+    }
+}
+
+void PokePi_GUI_init(){
+        /*Create a Tab view object*/
+    lv_obj_t *tabview;
+    tabview = lv_tabview_create(lv_scr_act(), LV_DIR_LEFT, 80);
+    //lv_obj_add_event_cb(lv_tabview_get_content(tabview), scroll_begin_event, LV_EVENT_SCROLL_BEGIN, NULL);
+
+    lv_obj_set_style_bg_color(tabview, lv_palette_lighten(LV_PALETTE_RED, 2), 0);
+
+    lv_obj_t * tab_btns = lv_tabview_get_tab_btns(tabview);
+    lv_obj_set_style_bg_color(tab_btns, lv_palette_darken(LV_PALETTE_GREY, 3), 0);
+    lv_obj_set_style_text_color(tab_btns, lv_palette_lighten(LV_PALETTE_GREY, 5), 0);
+    lv_obj_set_style_border_side(tab_btns, LV_BORDER_SIDE_RIGHT, LV_PART_ITEMS | LV_STATE_CHECKED);
+
+    /*Add 3 tabs (the tabs are page (lv_page) and can be scrolled*/
+    lv_obj_t *tab1 = lv_tabview_add_tab(tabview, "Tab 1");
+    lv_obj_t *tab2 = lv_tabview_add_tab(tabview, "Tab 2");
+    lv_obj_t *tab3 = lv_tabview_add_tab(tabview, "Tab 3");
+    lv_obj_t *tab4 = lv_tabview_add_tab(tabview, "Tab 4");
+    lv_obj_t *tab5 = lv_tabview_add_tab(tabview, "Tab 5");
+
+    lv_obj_set_style_bg_color(tab2, lv_palette_lighten(LV_PALETTE_AMBER, 3), 0);
+    lv_obj_set_style_bg_opa(tab2, LV_OPA_COVER, 0);
+
+    /*Add content to the tabs*/
+
+    lv_obj_t * btn1 = lv_btn_create(tab1, NULL);
+    lv_obj_set_event_cb(btn1, event_handler);
+    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -40);
+    label = lv_label_create(btn1, NULL);
+    lv_label_set_text(label, "Scan!");
+
+    
+    ta  = lv_textarea_create(tab2, NULL);
+    lv_obj_align(ta, NULL, LV_ALIGN_IN_TOP_MID, 0, LV_DPI / 16);
+    lv_obj_set_event_cb(ta, ta_event_cb);
+    lv_textarea_set_text(ta, "");
+    lv_coord_t max_h = LV_VER_RES / 2 - LV_DPI / 8;
+    if(lv_obj_get_height(ta) > max_h) lv_obj_set_height(ta, max_h);
+    kb_create();
+
+
+    static const char * btns[] ={"Exit", "Cancel", ""};
+    lv_obj_t * mbox1 = lv_msgbox_create(tab3, NULL);
+    lv_msgbox_set_text(mbox1, "Are you sure you want to exit PokePi?");
+    lv_msgbox_add_btns(mbox1, btns);
+    lv_obj_set_width(mbox1, 200);
+    lv_obj_set_event_cb(mbox1, exit_button_event_handler);
+    lv_obj_align(mbox1, NULL, LV_ALIGN_CENTER, 0, 0); /*Align to the corner*/
+
+    label = lv_label_create(tab4);
+    lv_label_set_text(label, "Forth tab");
+
+    label = lv_label_create(tab5);
+    lv_label_set_text(label, "Fifth tab");
+
+    //lv_obj_clear_flag(lv_tabview_get_content(tabview), LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_scroll_to_view_recursive(label, LV_ANIM_ON);
+
+}
+
+
--- src/PokePi/Graphics/Graphics.h
+++ src/PokePi/Graphics/Graphics.h
@@ -3,3 +3,12 @@
 
 extern void PokePi_GUI_init();
 //Inits the first GUI screen upon startup of PokePi. 
+
+
+static lv_obj_t * kb;
+static lv_obj_t * ta;
+static void kb_event_cb(lv_obj_t * keyboard, lv_event_t e);
+static void kb_create(void);
+static void ta_event_cb(lv_obj_t * ta_local, lv_event_t e);
+
+static void exit_button_event_handler(lv_obj_t * obj, lv_event_t event);
