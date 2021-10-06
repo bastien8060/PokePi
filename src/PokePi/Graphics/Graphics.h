da601dadb3d2528c61c82729b4cc2f805c202bc0
Bastien Saidi <saidi.ireland@gmail.com>
2021-10-06 23:26:22 +0100

    Fixed missing quote in include

--- src/PokePi/Graphics/Graphics.h
+++ src/PokePi/Graphics/Graphics.h
@@ -1,5 +1,5 @@
 #include "../../lvgl/lvgl.h"
-#include "../../lv_demos/lv_demo.h
+#include "../../lv_demos/lv_demo.h"
 
 extern void PokePi_GUI_init(){}
 //Inits the first GUI screen upon startup of PokePi. 
